3972e45c93cb4c04ee840b18cfb9c32ef93e4ede
miurakazuma67 <68637266+miurakazuma67@users.noreply.github.com>
2021-06-15 15:01:34 +0900

    Initial Commit

---
+++ Textview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Textview
+//
+//  Created by 三浦　一真 on 2021/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
